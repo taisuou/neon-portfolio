a2be581d377451ef7fe31a80b72ff5862560c9bb
sorashin <naka52shin@gmail.com>
2022-08-16 08:48:25 +0900

    add scale property to ground

--- src/components/three/Ground.tsx
+++ src/components/three/Ground.tsx
@@ -14,23 +14,24 @@ export const Ground: VFC = () => {
     '/SurfaceImperfections003_1K_Normal.jpg',
   ]);
   const groundProps = useControls('GroundControl',{
-    rotateX: { value: 0, min: -180, max: 180, step:1 },
+    rotateX: { value: -20, min: -180, max: 180, step:1 },
     rotateY: { value: 0, min: -180, max: 180, step:1 },
-    rotateZ: { value: 90, min: -180, max: 180, step:1 },
+    rotateZ: { value: 140, min: -180, max: 180, step:1 },
     posX: { value: 0, min: -5, max: 5, step:0.1 },
     posY: { value: 0, min: -5, max: 5, step:0.1 },
     posZ: { value: -0.9, min: -5, max: 5, step:0.1 },
-    blur: { value: 500, min: 0, max: 1000, step:1 },
-    mixBlur: { value: 12, min: 0, max: 100, step:1 },
+    blur: { value: 60, min: 0, max: 1000, step:1 },
+    mixBlur: { value: 100, min: 0, max: 500, step:1 },
+    scale: { value: 5, min: 0, max: 10, step:1 },
   })
 
   return (
     <>
       <mesh
         
-        rotation={[Math.PI * groundProps.rotateX/180, Math.PI * groundProps.rotateY/180, groundProps.rotateZ/180]}
+        rotation={[Math.PI * groundProps.rotateX/90, Math.PI * groundProps.rotateY/90, groundProps.rotateZ/90]}
         position={[groundProps.posX,groundProps.posY, groundProps.posZ]}
-        // scale={5}
+        scale={groundProps.scale}
       >
         <planeGeometry
           args={[8, 8]}
