58adde45e67d50656237597ae2324a332250b82b
sorashin <naka52shin@gmail.com>
2022-08-16 07:35:00 +0900

    [WIP] add debugger to ground

--- src/components/three/Ground.tsx
+++ src/components/three/Ground.tsx
@@ -4,6 +4,7 @@ import { Plane, Reflector, useTexture } from '@react-three/drei';
 import { useFrame } from '@react-three/fiber';
 import { MeshReflectorMaterial } from '@react-three/drei';
 import * as THREE from 'three';
+import { useControls } from 'leva';
 
 export const Ground: VFC = () => {
   useFrame(() => {});
@@ -12,14 +13,24 @@ export const Ground: VFC = () => {
     '/SurfaceImperfections003_1K_var1.jpg',
     '/SurfaceImperfections003_1K_Normal.jpg',
   ]);
+  const groundProps = useControls('GroundControl',{
+    rotateX: { value: 0, min: -180, max: 180, step:1 },
+    rotateY: { value: 0, min: -180, max: 180, step:1 },
+    rotateZ: { value: 90, min: -180, max: 180, step:1 },
+    posX: { value: 0, min: -5, max: 5, step:0.1 },
+    posY: { value: 0, min: -5, max: 5, step:0.1 },
+    posZ: { value: -0.9, min: -5, max: 5, step:0.1 },
+    blur: { value: 500, min: 0, max: 1000, step:1 },
+    mixBlur: { value: 12, min: 0, max: 100, step:1 },
+  })
 
   return (
     <>
       <mesh
         
-        rotation={[(-Math.PI * 1) / 180, 0, Math.PI / 2]}
-        position-z={-0.9}
-        scale={5}
+        rotation={[Math.PI * groundProps.rotateX/180, Math.PI * groundProps.rotateY/180, groundProps.rotateZ/180]}
+        position={[groundProps.posX,groundProps.posY, groundProps.posZ]}
+        // scale={5}
       >
         <planeGeometry
           args={[8, 8]}
@@ -27,8 +38,8 @@ export const Ground: VFC = () => {
         
           <MeshReflectorMaterial
             resolution={1024}
-            blur={[1000, 200]}
-            mixBlur={50}
+            blur={[groundProps.blur, groundProps.blur]}
+            mixBlur={groundProps.mixBlur}
             mixStrength={1.5}
             color="#f0f0f0"
             mirror={1}
