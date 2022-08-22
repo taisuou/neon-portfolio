cc094ab3093b723a41757238e7da7405a4e4f08c
sorashin <naka52shin@gmail.com>
2022-08-23 06:34:07 +0900

    Groundを少し柔らかく

--- src/components/three/Ground.tsx
+++ src/components/three/Ground.tsx
@@ -14,15 +14,18 @@ export const Ground: VFC = () => {
     '/SurfaceImperfections003_1K_Normal.jpg',
   ]);
   const groundProps = useControls('GroundControl', {
-    rotateX: { value: -90, min: -180, max: 180, step: 1 },
+    rotateX: { value: -86, min: -180, max: 180, step: 1 },
     rotateY: { value: 0, min: -180, max: 180, step: 1 },
     rotateZ: { value: 140, min: -180, max: 180, step: 1 },
     posX: { value: 0, min: -5, max: 5, step: 0.1 },
     posY: { value: -2, min: -5, max: 5, step: 0.1 },
     posZ: { value: -4.5, min: -5, max: 5, step: 0.1 },
-    blur: { value: 72, min: 0, max: 1000, step: 1 },
-    mixBlur: { value: 215, min: 0, max: 500, step: 1 },
+    blur: { value: 60, min: 0, max: 1000, step: 1 },
+    mixBlur: { value: 93, min: 0, max: 500, step: 1 },
     scale: { value: 6, min: 0, max: 10, step: 1 },
+    mixStrength: { value: -0.7, min: -5, max: 5, step: 0.1 },
+    color:'#f0f0f0',
+    resolution: { value: 1024, min: 0, max: 2048, step: 1 },
   });
 
   return (
@@ -39,16 +42,14 @@ export const Ground: VFC = () => {
         <planeGeometry args={[8, 8]} />
 
         <MeshReflectorMaterial
-          resolution={1024}
+          resolution={groundProps.resolution}
           blur={[groundProps.blur, groundProps.blur]}
           mixBlur={groundProps.mixBlur}
-          mixStrength={1.5}
-          color="#f0f0f0"
+          mixStrength={groundProps.mixStrength}
+          color={groundProps.color}
           mirror={1}
           metalness={0}
-          roughnessMap={floor}
-          normalMap={normal}
-          normalScale={new THREE.Vector2(2, 2)}
+          
         />
       </mesh>
     </>
