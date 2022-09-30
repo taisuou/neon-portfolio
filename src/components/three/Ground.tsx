0832f1c9b2682d089734afda14eeb1da0d1695e3
sorashin <naka52shin@gmail.com>
2022-09-30 16:21:54 +0900

    fix whiteout error

--- src/components/three/Ground.tsx
+++ src/components/three/Ground.tsx
@@ -1,4 +1,5 @@
 import { VFC } from 'react';
+import { useTexture } from '@react-three/drei';
 import { useFrame } from '@react-three/fiber';
 import { MeshReflectorMaterial } from '@react-three/drei';
 import { useControls } from 'leva';
@@ -6,6 +7,10 @@ import { useControls } from 'leva';
 export const Ground: VFC = () => {
   useFrame(() => {});
 
+  const [floor, normal] = useTexture([
+    '/SurfaceImperfections003_1K_var1.jpg',
+    '/SurfaceImperfections003_1K_Normal.jpg',
+  ]);
   const groundProps = useControls('GroundControl', {
     rotateX: { value: -86, min: -180, max: 180, step: 1 },
     rotateY: { value: 0, min: -180, max: 180, step: 1 },
@@ -15,7 +20,7 @@ export const Ground: VFC = () => {
     posZ: { value: 0, min: -5, max: 5, step: 0.1 },
     blur: { value: 400, min: 0, max: 1000, step: 1 },
     mixBlur: { value: 93, min: 0, max: 500, step: 1 },
-    radius: { value: 2.5, min: 0, max: 10, step: 0.1 },
+    radius:{value: 2.5, min: 0, max: 10, step: 0.1 },
     scale: { value: 6, min: 0, max: 10, step: 1 },
     mixStrength: { value: 0.5, min: -5, max: 5, step: 0.1 },
     color: '#aaaaaa',
